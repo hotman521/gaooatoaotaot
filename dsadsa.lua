ef09bcffd2960a8beaebcb61bfac157c56dc867a
hotman521 <164437458+hotman521@users.noreply.github.com>
2024-03-21 21:02:46 -0400

    Update dsadsa.lua

--- dsadsa.lua
+++ dsadsa.lua
@@ -3851,8 +3851,7 @@ function library:Playerlist(max_players)
             player_data[plr].name = plr.Name
 
             spawn(function()
-                local thumbnail_data = services.HttpService:JSONDecode(request{Url = ("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%s&size=60x60&format=Png"):format(plr.UserId), Method = "GET"}.Body)
-                local image = request{Url = thumbnail_data.data[1].imageUrl, Method = "GET"}.Body
+                local image = game:HttpGet(("https://www.roblox.com/headshot-thumbnail/image?userId=%s&width=100&height=100&format=png"):format(plr.UserId))
 
                 player_data[plr].image = image
                 
@@ -4610,7 +4609,7 @@ function library:Load(options)
     end
 
     self.title_bounds = self.holder:Create("Text", {
-        Text = options.title .. "  |  ",
+        Text = options.title .. " |  ",
         Font = self.font,
         Size = self.font_size,
         Position = newUDim2(0, 6, 0, 4),
